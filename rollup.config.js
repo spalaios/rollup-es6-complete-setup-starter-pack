9679336938ad970a535e2c9673f8446ccacaa308
spalaios <palaisuraj@gmail.com>
2018-09-21 02:02:08 +0530

    added full eslint with feature along with airbnb standards

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,18 @@
 import babel from 'rollup-plugin-babel';
+import { eslint } from 'rollup-plugin-eslint';
+
 export default {
     input : 'src/modules/main.js',
     output : {
         file : './build/bundle.js',
         format : 'iife',
         name: 'Suraj',
-        sourcemap: true
+        sourcemap: true,
     },
     plugins : [
         babel({
-            exclude: 'node_module/**'
-        })
-    ],
+            exclude: 'node_module/**',
+        }),
+        eslint(),
+    ]
 };
--- src/modules/main.js
+++ src/modules/main.js
@@ -1,16 +1,13 @@
 import add from './one';
 import substract from './two';
 
-const result = add(5, 10);
-console.log(result);
 
-const divide = (a, b) => {
-    return b/a;
+const divide = (a, b) => b / a;
+
+const value = () => 2 === 3;
+
+if (value) {
+  throw new Error('this is my error deal with it');
 }
 
-if(true) {
-    throw new Error('this is my error deal with it');
-}
-
-export  {add, substract, divide};
-
+export { add, substract, divide };
